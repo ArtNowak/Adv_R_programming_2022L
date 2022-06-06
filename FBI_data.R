9d88eb1a8c078bc5106d9559df745b5c8f200c7c
Artur <nowak.artr@gmail.com>
2022-06-07 00:08:05 +0200

    Functions cleaning data

--- FBI_data.R
+++ FBI_data.R
@@ -10,15 +10,18 @@ get_api_data <- function(pages, person_classification = '', status = '') {
 
   if (!is.character(person_classification)) {
     stop('Person_classification must be a string!')
-  } else if ( !(person_classification %in% c('', 'Main', 'Victim', 'Accomplice'))) {
+  } else if ( !(person_classification %in% c('', 'Main', 'Victim',
+                                             'Accomplice'))) {
     stop('Invalid value. Valid values: Main, Victim or Accomplice')
   }
   
   if (!is.character(status)) {
     stop('Status must be a string!')
-  } else if ( !(person_classification %in% c('', 'na', 'captured', 'recovered',
-                                             'located', 'surrendered', 'deceased'))) {
-    stop('Invalid value. Valid values: na, captured, recovered, located, surrendered, deceased')
+  } else if ( !(person_classification %in% c('', 'na', 'captured',
+                                             'recovered', 'located',
+                                             'surrendered', 'deceased'))) {
+    stop('Invalid value. Valid values: na, captured, recovered, located,
+         surrendered, deceased')
   }
   
   range <- pages/50
@@ -26,7 +29,8 @@ get_api_data <- function(pages, person_classification = '', status = '') {
   for (i in 1:range) {
     # create the url
     url <- paste0('https://api.fbi.gov/@wanted?pageSize=50', '&page=', i,
-                  '&person_classification=', person_classification, '&status=', status)
+                  '&person_classification=', person_classification,
+                  '&status=', status)
     # get the response and convert to json
     response <- GET(url)
     data <- content(response, as = 'text')
@@ -41,8 +45,9 @@ get_api_data <- function(pages, person_classification = '', status = '') {
 }
 
 clean_api_data <- function(input_df) {
-  c('reward_text', 'publication', 'warning_message', 'field_offices', 'sex', 'scars_and_marks', 'build',
-    'nationality', 'caution', 'race', 'hair', 'weight', 'place_of_birth', 'status', 'eyes', 'languages',
+  c('reward_text', 'publication', 'warning_message', 'field_offices', 'sex',
+    'scars_and_marks', 'build', 'nationality', 'caution', 'race', 'hair',
+    'weight', 'place_of_birth', 'status', 'eyes',
     'possible_countries')
   
   input_df$reward <- parse_number(input_df[, 'reward_text'])
@@ -89,11 +94,64 @@ clean_date <- function(date_col) {
 }
 
 
+# split list variables
+library(dplyr)
+split_vector_cols <- function(col_names, df) {
+  for (i in col_names) {
+    #deal with empty lists
+    df[[i]][lapply(df[[i]], length) == 0] <- NA
+  }
+  for (i in col_names) {
+    # spread values into rows
+    df <- unnest_wider(df, col = i, names_sep = '_')
+  }
+  df <- df
+  return (df)
+}
 
-df_new <- df_test[, c('reward_text', 'publication', 'warning_message', 'field_offices', 'sex', 'scars_and_marks', 'build',
-                      'nationality', 'caution', 'race', 'hair', 'weight', 'place_of_birth', 'status', 'eyes', 'languages',
-                      'possible_countries')]
+# warning_messages
+library(stringr)
+clear_warning_message <- function(df) {
+  possible_threats <- c('FLIGHT RISK', 'DANGEROUS', 'ARMED', 'ESCAPE RISK',
+                        'EXTREMELY DANGEROUS', 'HIGH-RISK ARREST', 
+                        'VIOLENT TENDENCIES', 'MAY ABUSE DRUGS',
+                        'SUICIDAL TENDENCIES', 'BI-POLAR',
+                        'IN NEED OF MEDICATIONS', 'SUICIDE RISK')
+  for (i in possible_threats) {
+    df[[str_to_title(i)]] <- ifelse(grepl(i, df$warning_message), T, F)
+  }
+  df <- within(df, rm('warning_message'))
+  return(df)
+}
+
+
+df_new <- df_test[, c('reward_text', 'publication', 'warning_message',
+                      'field_offices', 'sex', 'scars_and_marks', 'build',
+                      'nationality', 'race', 'hair', 'weight', 'place_of_birth',
+                      'status', 'eyes', 'possible_countries',
+                      'height_max', 'height_min')]
+
+#height
+df_new$height <- lapply((df_new$height_max + df_new$height_min)/2, as.numeric)
+df_new <- subset(df_new, select = -c(height_max, height_min))
 # reward
 df_new$new_reward <- lapply(df_new$reward_text, clean_reward)
+df_new <- subset(df_new, select = -c(reward_text))
 # date
-df_new <- subset(cbind(df_new, clean_date(df_new$publication)), select = -c(publication))
+df_new <- subset(cbind(df_new, clean_date(df_new$publication)),
+                 select = -c(publication))
+# vector cols
+df_new <- split_vector_cols(c('field_offices', 'possible_countries'), df_new)
+# warning_message
+df_new <- clear_warning_message(df_new)
+# todo - dummies
+
+# scars_and_marks
+df_new$scars_and_marks <- ifelse(!is.na(df_new$scars_and_marks), T, F)
+#weight
+# to do - naprawic xD bo sa rozne wartosci, wez jakis parse_number czy cos
+# i jak sa 2 wartosci to srednia wyciagnij idk
+df_new$weight <- sub(" pounds.*", "", df_new$weight)
+
+#place of birth
+df_new$place_of_birth <- sapply(strsplit(df_new[['place_of_birth']], ', '), tail, 1)
